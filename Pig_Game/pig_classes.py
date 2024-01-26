b88783f262307de3a0e0179368a56cefd8385ebb
Ethan <Ethandrover@hotmail.com>
2024-01-26 08:33:54 -0330

    ...

--- Pig_Game/pig_classes.py
+++ Pig_Game/pig_classes.py
@@ -1,4 +1,22 @@
+
+import random
+from dataclasses import dataclass
 @Dataclass
-class PigClass:
-    def play_pig(self):
-        self.play_pig()
+class Die:
+    __value: int = 1
+
+    def roll(self):
+        self.__value = random.randint(1, 6)
+    @property
+    def get_value(self):
+        return self.__value
+class game:
+    def player_turn(self):
+        while True:
+            playerpoints = 0
+
+
+
+
+
+
---
+++ Pig_Game/pig_game.py
@@ -0,0 +1,10 @@
+import random
+
+def pig_game():
+    print("Let's Play PIG!!")
+    print("Here are the rules!"
+          "\n* See how many turns it takes you to get to 20."
+          "\n* Turn ends when you hold or roll a 1."
+          "\n* If you roll a 1, you lose all your points for that turn."
+          "\n* If you hold, you save all points for that turn.\n")
+

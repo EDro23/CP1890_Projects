6a3578f985b5dda5d353dca12b33e447f1d965f9
Ethan <Ethandrover@hotmail.com>
2024-01-29 23:44:34 -0330

    Edit player option added.

--- Baseball_Application_Classes.py
+++ Baseball_Application_Classes.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 POSITIONS = ('C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'P')
 
+
 def dateandtime():
     print("CURRENT DATE: ", datetime.now().strftime("%y/%m/%d"))
     gamedate = input("GAME DATE: \t")
-    #days_until_game = datetime.strftime(gamedate, "%y/%m/%d").date() - datetime.now().strftime("%y/%m/%d")
-
+    # days_until_game = datetime.strftime(gamedate, "%y/%m/%d").date() - datetime.now().strftime("%y/%m/%d")
 
 
 def add_player(players):
@@ -16,11 +16,12 @@ def add_player(players):
     position = get_player_position()
     at_bats = get_at_bats()
     hits = get_hits(at_bats)
-    #full_name = f'{first_name} {last_name}'
+    # full_name = f'{first_name} {last_name}'
 
     player = BaseballPlayer(first_name, last_name, position, at_bats, hits)
     players.append(player)
     print(f'Player {player.first_name} {player.last_name} was added.')
+
 
 def get_player_position():
     while True:
@@ -30,6 +31,7 @@ def get_player_position():
         else:
             print('Invalid position. Please enter correctly.')
             print('Valid positions are: ' + ', '.join(POSITIONS))
+
 
 def get_at_bats():
     while True:
@@ -43,6 +45,7 @@ def get_at_bats():
         else:
             return at_bats
 
+
 def get_hits(at_bats):
     while True:
         try:
@@ -55,19 +58,22 @@ def get_hits(at_bats):
         else:
             return hits
 
+
 def display_lineup(players):
-    if players == None:
+    if players is None:
         print('No players in lineup.')
     else:
         print(f"{'':3}{'Player':40}{'POS':6}{'AB':>4}\t{'HITS':>6}{'AVG':>8}")
-        print('-'*80)
+        print('-' * 80)
         for i, player in enumerate(players):
-            print(f'{i+1:<3d}{player.first_name.ljust(1)} {player.last_name.ljust(29)} {player.position.rjust(6)}\t{str(player.at_bats).rjust(6)} {str(player.hits).rjust(6)}\t\t{player.battingAvg:.3f}')
+            print(
+                f'{i + 1:<3d}{player.first_name.ljust(1)} {player.last_name.ljust(29)} {player.position.rjust(6)}\t{str(player.at_bats).rjust(6)} {str(player.hits).rjust(6)}\t\t{player.batting_avg:.3f}')
         print()
+
 
 def remove_player(players):
     try:
-        if players == None:
+        if players is None:
             print("No players in lineup")
         else:
             choice = input(f'\nWho would you like to remove? (Select by first name, CASE SENSITIVE): ')
@@ -115,32 +121,61 @@ def move_player(players):
         print('Invalid, please try again.')
 
 
+def edit_player_position(players, POSITIONS):
+    while True:
+        try:
+            if not players:
+                print('Player not in lineup.\nPlease try again.')
+            else:
+                choice = input("Who would you like to edit the position for? (Enter first name): ")
+                # Check if the entered name is in any player's first name
+                if not any(player.first_name == choice for player in players):
+                    print('This player does not exist. Try again.')
+                else:
+                    print('Valid positions are: ' + ', '.join(POSITIONS))
+                    poschoice = input(f"\nWhich position would you like to move {choice} to?: ")
+                    if poschoice.upper() not in POSITIONS:
+                        print('Invalid position. Please try again.')
+                    else:
+                        for player in players:
+                            if player.first_name == choice.title():
+                                player.position = poschoice.upper()
+                                print(f"Position has been successfully changed for {player.first_name}.")
+                        break
+        except Exception:
+            print('Invalid, please try again.')
+
+
 def display_seperator():
-    print('-'*80)
+    print('-' * 80)
+
 
 def display_title():
     print('\t\t\t\t Baseball Team Manager')
 
+
 def display_options():
     print('Menu Options')
     print(" 1 - Display lineup\n",
-     "2 - Add player\n",
-     "3 - Remove player\n",
-     "4 - Move player\n",
-     "5 - Edit player position\n",
-     "6 - Edit player stats\n",
-     "7 - Exit program\n")
+          "2 - Add player\n",
+          "3 - Remove player\n",
+          "4 - Move player\n",
+          "5 - Edit player position\n",
+          "6 - Edit player stats\n",
+          "7 - Exit program\n")
 
-def display_positions():
+
+def display_positions(POSITIONS):
     print('POSITIONS')
     print(','.join(POSITIONS))
+
 
 def main():
     display_seperator()
     display_title()
     dateandtime()
     display_options()
-    display_positions()
+    display_positions(POSITIONS)
     display_seperator()
 
     players = []
@@ -159,13 +194,14 @@ def main():
             remove_player(players)
         elif choice == 4:
             move_player(players)
+        elif choice == 5:
+            edit_player_position(players,POSITIONS)
         elif choice == 7:
             print("Bye!")
             break
         else:
             print("Invalid choice. Please try again.")
 
+
 if __name__ == "__main__":
     main()
-
-
--- baseballClasses.py
+++ baseballClasses.py
@@ -11,21 +11,23 @@ class BaseballPlayer:
 
     @property
     def fullname(self):
-        return f'{self.firstname} {self.lastname}'
+        return f'{self.first_name} {self.last_name}'
 
     @property
-    def battingAvg(self) -> float:
+    def batting_avg(self) -> float:
         try:
             avg = self.hits / self.at_bats
             return avg
         except ZeroDivisionError:
             return 0.0
 
+
 def test_func():
-    player1 = BaseballPlayer('Ethan','Drover','S',10,10)
+    player1 = BaseballPlayer('Ethan', 'Drover', 'S', 10, 10)
     print(f"Player: {player1.fullname}")
-    print(f"Batting Avg: {player1.battingAvg}")
+    print(f"Batting Avg: {player1.batting_avg}")
     print('Test complete')
+
 
 if __name__ == '__main__':
     test_func()
